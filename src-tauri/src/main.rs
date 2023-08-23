556bfd419a3a73c50c189e43e40ffad118145515
Matthias <tuttiacc@hotmail.com>
2023-08-23 15:31:21 +0200

    fixed happ path

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -262,7 +262,7 @@ pub async fn install_app_if_necessary(
             .map_err(|e| AppError::ConductorApiError(e))?;
 
         // replace-me --- replace the path with the correct path to your .happ file here
-        let app_bundle = AppBundle::decode(include_bytes!("../../pouch/word-condenser.happ"))
+        let app_bundle = AppBundle::decode(include_bytes!("../../pouch/replace-me.happ"))
             .map_err(|e| AppError::AppBundleError(e))?;
 
         admin_ws
