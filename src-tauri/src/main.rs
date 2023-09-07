bdb58d55c601eeeead366b7ccaef9edb0f7f16e8
Matthias <tuttiacc@hotmail.com>
2023-09-07 11:43:41 +0200

    fixed network seed const

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -31,8 +31,8 @@ pub const WINDOW_TITLE: &str = "replace-me"; // Title of the window
 pub const WINDOW_WIDTH: f64 = 1400.0; // Default window width when the app is opened
 pub const WINDOW_HEIGHT: f64 = 880.0; // Default window height when the app is opened
 const PASSWORD: &str = "pass"; // Password to the lair keystore
-pub const DEFAULT_NETWORK_SEED: Option<String> = None;  // replace-me (optional): Depending on your application, you may want to put a network seed here or
-                                            // read it secretly from an environment variable. If so, replace `None` with `Some(String::from([your network seed here]))`
+pub const DEFAULT_NETWORK_SEED: Option<&str> = None;  // replace-me (optional): Depending on your application, you may want to put a network seed here or
+                                            // read it secretly from an environment variable. If so, replace `None` with `Some("your network seed here")`
 const SIGNALING_SERVER: &str = "wss://signal.holo.host"; // replace-me (optional): Change the signaling server if you want
 
 
@@ -215,7 +215,7 @@ pub async fn launch(
 
     let network_seed = match fs.read_profile_network_seed() {
         Some(seed) => Some(seed),
-        None => DEFAULT_NETWORK_SEED,
+        None => DEFAULT_NETWORK_SEED.map(|s| s.to_string()),
     };
 
     install_app_if_necessary(network_seed, &mut admin_ws).await?;
