245e3ff61274f99376bbd2bbe272eed067778627
Matthias <tuttiacc@hotmail.com>
2023-09-12 16:37:24 +0200

    indentation fixes

--- src-tauri/src/commands/restart.rs
+++ src-tauri/src/commands/restart.rs
@@ -2,9 +2,9 @@
 // restarts the Holochain Launcher
 #[tauri::command]
 pub fn restart(
-  app_handle: tauri::AppHandle,
+    app_handle: tauri::AppHandle,
 ) -> Result<(), String> {
-  log::warn!("A Restart of the app has been requested. Restarting...");
-  app_handle.restart();
-  Ok(())
+    log::warn!("A Restart of the app has been requested. Restarting...");
+    app_handle.restart();
+    Ok(())
 }
