cfcb00550caf634ad63d3e77bca74b2f98f0314d
Collins Muriuki <hello@collinsmuriuki.xyz>
2024-08-19 22:56:02 +0300

    update binary installation script

--- setup/install_binaries.sh
+++ setup/install_binaries.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+PLATFORM=$(rustc -vV | sed -n 's/^.*host: \(.*\)*$/\1/p')
 REQUIRED_HOLOCHAIN_VERSION="0.3.2"
 REQUIRED_LAIR_VERSION="0.4.5"
+
+if [[ "$PLATFORM" == *windows* ]]; then
+  HOLOCHAIN_BINARY_FILENAME="holochain-v$REQUIRED_HOLOCHAIN_VERSION-$PLATFORM.exe"
+  LAIR_BINARY_FILENAME="lair-keystore-v$REQUIRED_LAIR_VERSION-$PLATFORM.exe"
+else
+  HOLOCHAIN_BINARY_FILENAME="holochain-v$REQUIRED_HOLOCHAIN_VERSION-$PLATFORM"
+  LAIR_BINARY_FILENAME="lair-keystore-v$REQUIRED_LAIR_VERSION-$PLATFORM"
+fi
+
+DESTINATION_DIR="src-tauri/bins"
 
 # Check that this script is being run from the right location
 if [ ! -f "package.json" ] || [ ! -f "src-tauri/tauri.conf.json" ];
@@ -9,7 +20,6 @@ then
     echo "Error: You must run this script in the root directory of the kangaroo repository."
     exit 1
 fi
-
 # Check wheter cargo is available
 if [ ! command -v cargo &> /dev/null ] || [ ! command -v rustc &> /dev/null ];
 then
@@ -17,37 +27,34 @@ then
     exit 1
 fi
 
-# get target architecture triple, e.g. unknown-linux-gnu on Ubuntu 22.04
-TARGET_TRIPLE=$(rustc -vV | sed -n 's/^.*host: \(.*\)*$/\1/p')
-
 # create src-tauri/bins if id doesn't exist
-if [ ! -d src-tauri/bins ];
-    then mkdir src-tauri/bins
+if [ ! -d $DESTINATION_DIR ];
+    then mkdir $DESTINATION_DIR
 fi
 
 # check whether correct holochain binary is already in the src-tauri/bins folder
-if [ -f "src-tauri/bins/holochain-v${REQUIRED_HOLOCHAIN_VERSION}-$TARGET_TRIPLE" ];
-    then echo "Required holochain binary already installed."
+if [ -f "$DESTINATION_DIR/$HOLOCHAIN_BINARY_FILENAME" ];
+    then
+        echo "Required holochain binary already installed."
     else
-    	echo "Installing required holochain binary from crates.io"
-    	echo "Running command 'cargo install holochain --version $REQUIRED_HOLOCHAIN_VERSION --locked --features sqlite-encrypted'"
-        cargo install holochain --version $REQUIRED_HOLOCHAIN_VERSION --locked --features sqlite-encrypted
-        echo "Copying holochain binary to src-tauri/bins folder."
-        HOLOCHAIN_PATH=$(which holochain)
-        cp $HOLOCHAIN_PATH src-tauri/bins/holochain-v${REQUIRED_HOLOCHAIN_VERSION}-$TARGET_TRIPLE
+        echo "Installing required holochain binary from matthme/holochain-binaries."
+        HOLOCHAIN_BINARIES_URL="https://github.com/matthme/holochain-binaries/releases/download/holochain-binaries-$REQUIRED_HOLOCHAIN_VERSION/$HOLOCHAIN_BINARY_FILENAME"
+        curl -L $HOLOCHAIN_BINARIES_URL -o $DESTINATION_DIR/$HOLOCHAIN_BINARY_FILENAME
+        chmod +x $DESTINATION_DIR/$HOLOCHAIN_BINARY_FILENAME
+        echo "holochain binary downloaded and save to $DESTINATION_DIR/$HOLOCHAIN_BINARY_FILENAME"
 fi
 
 # check whether correct lair binary is already in the src-tauri/bins folder
-if [ -f "src-tauri/bins/lair-keystore-v${REQUIRED_LAIR_VERSION}-${TARGET_TRIPLE}" ];
+if [ -f "$DESTINATION_DIR/$LAIR_BINARY_FILENAME" ];
 
-    then echo "Required lair-keystore binary already installed."
+    then
+        echo "Required lair-keystore binary already installed."
     else
     	echo "Installing required lair-keystore binary from crates.io"
-    	echo "Running command 'cargo install lair-keystore --version $REQUIRED_LAIR_VERSION --locked'"
-        cargo install  lair_keystore --version $REQUIRED_LAIR_VERSION --locked
-        echo "Copying lair-keystore binary to src-tauri/bins folder."
-        LAIR_PATH=$(which lair-keystore)
-        cp $LAIR_PATH src-tauri/bins/lair-keystore-v${REQUIRED_LAIR_VERSION}-$TARGET_TRIPLE
+        LAIR_BINARIES_URL="https://github.com/matthme/holochain-binaries/releases/download/lair-binaries-$REQUIRED_LAIR_VERSION/$LAIR_BINARY_FILENAME"
+        curl -L $LAIR_BINARIES_URL -o $DESTINATION_DIR/$LAIR_BINARY_FILENAME
+        chmod +x $DESTINATION_DIR/$LAIR_BINARY_FILENAME
+        echo "lair binary downloaded and saved to $DESTINATION_DIR/$LAIR_BINARY_FILENAME"
 fi
 
 echo "done."
